71e0795cd1874484461f52ea2c02c8750d778eb9
Vedant Phougat <9568209+phougatv@users.noreply.github.com>
2025-07-03 23:39:55 +0530

    initial commit

---
+++ blog/2025-07-03-oop-refresher.md
@@ -0,0 +1,30 @@
+---
+slug: oop-refresher
+title: OOP Refresher
+authors: [phougatv]
+tags: [oop]
+toc_min_heading_level: 2
+toc_max_heading_level: 3
+hide_table_of_contents: false
+---
+
+## I. Introduction
+In this post we will refresh our knowledge of Object-Oriented Programming (OOP) concepts by answering interview style questions.
+
+---
+
+## II. Questions & Answers
+### 1. Association, Aggregation, and Composition
+- What is _association_, _aggregation_, and _composition_ and how do they differ from each other?
+- What real-world problems do they solve? Use an example to explain how they solve the problem?
+
+<!-- truncate -->
+
+### 2. Abstraction, Inheritance, Polymorphism, and Encapsulation
+- What are the four main pillars of OOP: _abstraction_, _inheritance_, _polymorphism_, and _encapsulation_?
+- What is _abstraction_? Explain the underlying problem it solves. Use an example to explain **how**.
+- What is _inheritance_? Explain the underlying problem it solves. Use an example to explain **how**.
+- What is _encapsulation_? Explain the underlying problem it solves. Use an example to explain **how**.
+- What is _polymorphism_? Explain the underlying problem it solves. Use an example to explain **how**.
+- Explain using an example how _abstraction_ and _encapsulation_ differ from each other?
+- What problems arise in systems that heavily rely on _inheritance_ and how we can solve these problems?
---
+++ docusaurus.config.ts
@@ -0,0 +1,154 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+    title: 'Evolving Engineer',
+    tagline: 'A blog about software engineering',
+    favicon: 'img/favicon.ico',
+
+    // Add stylesheets here
+    stylesheets: [
+        'https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&family=Spline+Sans+Mono:wght@400;700&display=swap',
+    ],
+
+    // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+    future: {
+        v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    },
+
+    // Set the production url of your site here
+    url: 'https://phougatv.github.io',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
+
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'phougatv', // Usually your GitHub org/user name.
+    projectName: 'phougatv.github.io', // Usually your repo name.
+
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
+
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    sidebarPath: './sidebars.ts',
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    //editUrl:
+                    //    'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                },
+                blog: {
+                    showReadingTime: true,
+                    feedOptions: {
+                        type: ['rss', 'atom'],
+                        xslt: true,
+                    },
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    //editUrl:
+                    //    'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                    // Useful options to enforce blogging best practices
+                    onInlineTags: 'warn',
+                    onInlineAuthors: 'warn',
+                    onUntruncatedBlogPosts: 'warn',
+                },
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
+    ],
+
+    themeConfig: {
+        // Replace with your project's social card
+        image: 'img/docusaurus-social-card.jpg',
+        navbar: {
+            title: 'Evolving Engineer',
+            logo: {
+                alt: 'Evolving Engineer Logo',
+                src: 'img/logo.svg',
+            },
+            items: [
+                {
+                    type: 'docSidebar',
+                    sidebarId: 'tutorialSidebar',
+                    position: 'left',
+                    label: 'Tutorial',
+                },
+                { to: '/blog', label: 'Blog', position: 'left' },
+                {
+                    href: 'https://github.com/facebook/docusaurus',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ]
+        },
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Docs',
+                    items: [
+                        {
+                            label: 'Tutorial',
+                            to: '/docs/intro',
+                        },
+                    ],
+                },
+                {
+                    title: 'Community',
+                    items: [
+                        {
+                            label: 'Stack Overflow',
+                            href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                        },
+                        {
+                            label: 'Discord',
+                            href: 'https://discordapp.com/invite/docusaurus',
+                        },
+                        {
+                            label: 'X',
+                            href: 'https://x.com/docusaurus',
+                        },
+                    ],
+                },
+                {
+                    title: 'More',
+                    items: [
+                        {
+                            label: 'Blog',
+                            to: '/blog',
+                        },
+                        {
+                            label: 'GitHub',
+                            href: 'https://github.com/facebook/docusaurus',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+            additionalLanguages: ['csharp', 'javascript', 'typescript', 'java', 'python', 'bash', 'json', 'yaml', 'css', 'graphql', 'markdown', 'diff', 'sql']
+        },
+    } satisfies Preset.ThemeConfig,
+};
+
+export default config;
